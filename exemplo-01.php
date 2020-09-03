a604930ef994160c82279b7a3860a6c0f25527d1
Hyago b <hyagobezerra64@gmail.com>
2020-09-03 17:57:02 -0300

    Abstrata

---
+++ exemplo-01.php
@@ -0,0 +1,40 @@
+<?php
+
+interface Veiculo {
+
+    public function acelerar($velocidade);
+    public function frenar($velocidade);
+    public function trocarMarcha($marcha);
+}
+
+//para implementar outras interfaces usa-se , ex: class Civic implements Veiculo, outrainterface {
+abstract class Automovel implements Veiculo {
+
+    public function acelerar($velocidade){
+        echo "O veiculo acelerou até " . $velocidade . " km/h";
+    }
+    public function frenar($velocidade){
+        echo "o veiculo Frenou até " . $velocidade . " km/k";
+    }
+    public function trocarMarcha($marcha){
+        echo "o veiculo engatou a marcha " . $marcha;
+    }
+
+}
+
+
+class DelRey extends Automovel {
+
+    public function empurrar(){
+
+
+    }
+
+}
+
+$carro = new DelRey();
+
+$carro ->acelerar(200);
+
+
+?>
